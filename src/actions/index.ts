8b3768dfdcb09fd985c481bdc5403684539ad3c3
severjason <ssever2010@gmail.com>
2018-04-04 02:01:33 +0300

    Added actions toggleNote, deleteNote, openModal, closeModal

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -2,5 +2,24 @@ import { types } from '../constants/types';
 import { AppAction } from '../interfaces';
 
 export const getNotes = (): AppAction => ({
-    type: types.GET_NOTES,
+    type: types.notes.GET_NOTES,
+});
+
+export const toggleNote = (id: string): AppAction => ({
+    type: types.notes.TOGGLE_NOTE,
+    id: id,
+});
+
+export const deleteNote = (id: string): AppAction => ({
+    type: types.notes.DELETE_NOTE,
+    id: id,
+});
+
+export const openModal = (modalProps: any): AppAction => ({
+    type: types.modal.OPEN_MODAL,
+    payload: modalProps,
+});
+
+export const closeModal = (): AppAction => ({
+    type: types.modal.CLOSE_MODAL,
 });
