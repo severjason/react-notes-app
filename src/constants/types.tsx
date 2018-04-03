716707b5b2b0195da2bdebe27a9580561d3a5b71
severjason <ssever2010@gmail.com>
2018-04-04 02:11:10 +0300

    Added new types

--- src/constants/types.tsx
+++ src/constants/types.tsx
@@ -1,3 +1,11 @@
 export const types = {
-    GET_NOTES: 'GET_NOTES',
+    notes: {
+        GET_NOTES: 'GET_NOTES',
+        TOGGLE_NOTE: 'TOGGLE_NOTE',
+        DELETE_NOTE: 'DELETE_NOTE',
+    },
+    modal: {
+        OPEN_MODAL: 'OPEN_MODAL',
+        CLOSE_MODAL: 'CLOSE_MODAL',
+    }
 };
