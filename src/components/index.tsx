ce0218768ba50caac01bdb94f47c52351532c198
severjason <ssever2010@gmail.com>
2018-04-04 02:10:57 +0300

    Added EmptyPage to index

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,9 +1,11 @@
 import Header from './Header';
 import NotesList from './NotesList';
 import Note from './Note';
+import EmptyPage from './EmptyPage';
 
 export {
     Header,
     NotesList,
     Note,
+    EmptyPage,
 };
