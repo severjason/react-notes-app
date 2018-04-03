445c68999f255d69ced109713a2ac0c7f9a224ec
severjason <ssever2010@gmail.com>
2018-04-04 02:06:58 +0300

    Added ModalManager to index

--- src/containers/index.tsx
+++ src/containers/index.tsx
@@ -1,9 +1,11 @@
 import App from './App';
 import Home from './Home';
 import Routes from './Routes';
+import ModalManager from './ModalManager';
 
 export {
     App,
     Home,
     Routes,
+    ModalManager,
 };
