9bf01044f7e5502a0f6d068b104baabd826dc49c
severjason <ssever2010@gmail.com>
2018-04-04 02:05:24 +0300

    Added AppActions interface

--- src/interfaces/index.tsx
+++ src/interfaces/index.tsx
@@ -1,17 +1,28 @@
 export interface AppAction {
     type: string;
-    id?: number;
+    id?: string;
+    payload?: any;
+}
+
+export interface AppActions {
+    getNotes(): AppAction;
+    toggleNote(id: string): AppAction;
+    deleteNote(id: string): AppAction;
+    openModal(modalProps: any): AppAction;
+    closeModal(): AppAction;
 }
 
 export interface AppNote {
-    id: number;
+    id: string;
     title: string;
     categories: string[];
     color: string;
     tags: string[];
     text: string;
+    expanded: boolean;
 }
 
 export interface AppState {
     notes: AppNote[];
+    modals: any;
 }
