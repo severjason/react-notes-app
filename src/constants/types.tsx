edd0901b270f24fda05fc97a27cc1452afd1eb5f
severjason <ssever2010@gmail.com>
2018-04-05 03:16:03 +0300

    Added ADD_NOTE, UPDATE_NOTE and OPEN_MODAL_FOR_UPDATE types

--- src/constants/types.tsx
+++ src/constants/types.tsx
@@ -2,10 +2,13 @@ export const types = {
     notes: {
         GET_NOTES: 'GET_NOTES',
         TOGGLE_NOTE: 'TOGGLE_NOTE',
+        ADD_NOTE: 'ADD_NOTE',
+        UPDATE_NOTE: 'UPDATE_NOTE',
         DELETE_NOTE: 'DELETE_NOTE',
     },
     modal: {
         OPEN_MODAL: 'OPEN_MODAL',
+        OPEN_MODAL_FOR_UPDATE: 'OPEN_MODAL_FOR_UPDATE',
         CLOSE_MODAL: 'CLOSE_MODAL',
     }
 };
