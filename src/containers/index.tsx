8bee4e2ad98dc44c8aa64f118ab46ecd41ac25dd
severjason <ssever2010@gmail.com>
2018-04-05 03:18:05 +0300

    Renamed ModalManager -> NoteModal

--- src/containers/index.tsx
+++ src/containers/index.tsx
@@ -1,11 +1,11 @@
 import App from './App';
 import Home from './Home';
 import Routes from './Routes';
-import ModalManager from './ModalManager';
+import NoteModal from './NoteModal';
 
 export {
     App,
     Home,
     Routes,
-    ModalManager,
+    NoteModal,
 };
