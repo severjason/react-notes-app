0180663f70a6be70f2b080805f204e672df519b3
severjason <ssever2010@gmail.com>
2018-04-05 03:16:51 +0300

    Added CategoriesList to index

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -2,10 +2,12 @@ import Header from './Header';
 import NotesList from './NotesList';
 import Note from './Note';
 import EmptyPage from './EmptyPage';
+import CategoriesList from './CategoriesList';
 
 export {
     Header,
     NotesList,
     Note,
     EmptyPage,
+    CategoriesList,
 };
