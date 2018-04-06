ab2b00acd0460bf9efe2c98390d997b27874010a
severjason <ssever2010@gmail.com>
2018-04-06 12:28:59 +0300

    Created tagsReducer

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -2,12 +2,14 @@ import { combineReducers } from 'redux';
 import notesReducer from './notesReducer';
 import modalReducer from './modalReducer';
 import categoriesReducer from './categoriesReducer';
+import tagsReducer from './tagsReducer';
 import { AppState } from '../interfaces';
 
 const rootReducer = combineReducers<AppState>({
     notes: notesReducer,
     modal: modalReducer,
     categories: categoriesReducer,
+    tags: tagsReducer,
 });
 
 export default rootReducer;
