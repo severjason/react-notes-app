6f6e7e6c5f358f6b248bea47c2499960c5492fa8
severjason <ssever2010@gmail.com>
2018-04-06 12:37:31 +0300

    Added CategoriesList to index

--- src/containers/index.tsx
+++ src/containers/index.tsx
@@ -2,10 +2,12 @@ import App from './App';
 import Home from './Home';
 import Routes from './Routes';
 import NoteModal from './NoteModal';
+import CategoriesList from './CategoriesList';
 
 export {
     App,
     Home,
     Routes,
     NoteModal,
+    CategoriesList,
 };
