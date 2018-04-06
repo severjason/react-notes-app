6ee4fbdc0b19cca05927b64dfc31f0efedfa8c99
severjason <ssever2010@gmail.com>
2018-04-06 12:45:34 +0300

    Updated NoteModal container

--- src/containers/NoteModal/index.tsx
+++ src/containers/NoteModal/index.tsx
@@ -1,36 +1,51 @@
 import * as React from 'react';
-import { ChangeEvent } from 'react';
-import { Modal, Button, CheckboxProps, InputProps, TextAreaProps } from 'semantic-ui-react';
-import { AppActions, AppNote, AppNoteModal } from '../../interfaces';
+import { ChangeEvent, ReactNode } from 'react';
+import { Modal, Button, CheckboxProps, InputProps, TextAreaProps, Label, Icon } from 'semantic-ui-react';
+import { AppActions, AppCategories, AppNote, AppNoteModal, AppTags } from '../../interfaces';
 import { Form, Input, TextArea, Checkbox, Divider } from 'semantic-ui-react';
 import './index.css';
 import * as uuid from 'uuid';
+import * as helpers from '../../helpers';
 
 interface AppNoteModalProps {
     modal: AppNoteModal;
+    categories: AppCategories;
+    tags: AppTags;
 }
 
 interface AppNoteModalDispatch {
     actions: AppActions;
 }
 
+interface AppNoteModalState extends AppNote {
+    newTag: string;
+}
+
 export class NoteModal extends React.Component<AppNoteModalProps & AppNoteModalDispatch, {}> {
 
-    private INITIAL_STATE: AppNote = {
+    private INITIAL_STATE: AppNoteModalState = {
         id: '',
         title: '',
         color: 'black',
         text: '',
-        categories: [''],
-        tags: [''],
+        categories: [],
+        tags: [],
         expanded: false,
+        newTag: ''
     };
 
-    private colors: string[] = ['Black', 'Red', 'Green', 'Yellow'];
+    public state: AppNoteModalState = Object.assign({}, this.INITIAL_STATE);
+
+    private colors: string[] = ['black', 'red', 'green', 'orange', 'blue', 'purple', 'brown', 'violet', 'teal', 'pink'];
+
+    private categoriesList: string[] = this.props.categories.categoriesList
+        .filter((category: string) => category !== 'all');
 
     private maxTitleLength: number = 20;
 
-    public state: AppNote = Object.assign({}, this.INITIAL_STATE);
+    private maxNewTagLength: number = 15;
+
+    private maxTextAreaLength: number = 400;
 
     componentDidUpdate() {
         if (this.props.modal.openedForUpdate && this.props.modal.modalProps.id !== this.state.id) {
@@ -42,26 +57,54 @@ export class NoteModal extends React.Component<AppNoteModalProps & AppNoteModalD
         this.setState({color: value});
     }
 
-    private handleTitleChange = (e: ChangeEvent<HTMLInputElement>, {value}: InputProps) => {
+    private handleTitleChange = (e: ChangeEvent<HTMLInputElement>, {value}: InputProps): void => {
         this.setState({title: value});
     }
 
-    private handleTextChange = (e: ChangeEvent<HTMLTextAreaElement>, {value}: TextAreaProps) => {
+    private handleTextChange = (e: ChangeEvent<HTMLTextAreaElement>, {value}: TextAreaProps): void => {
         this.setState({text: value});
     }
 
-    private resetState = (): void => {
-        this.setState(this.INITIAL_STATE);
+    private handleCategoryChange = (e: ChangeEvent<HTMLInputElement>, {value}: any): void => {
+        this.setState({categories: helpers.toggleStringInArray(this.state.categories, value)});
     }
 
-    private isDisabled = (): boolean => !(this.state.title);
+    private handleTagClick = (clickedTag: string): void => {
+        this.setState({tags: helpers.toggleStringInArray(this.state.tags, clickedTag)});
+    }
+
+    private handleNewTagChange = (e: ChangeEvent<HTMLInputElement>, {value}: InputProps): void => {
+        this.setState({newTag: value});
+    }
+
+    private handleDeleteTag = (tag: string): void => {
+        this.props.actions.deleteCustomTag(tag);
+    }
+
+    private resetState = (): void => this.setState(this.INITIAL_STATE);
+
+    private resetNewTag = (): void => this.setState({newTag: ''});
+
+    private addNoteIsDisabled = (): boolean => !(this.state.title);
+
+    private addTagIsDisabled = (tags: string[]): boolean => tags.includes(this.state.newTag.toLowerCase());
+
+    private removeNewTagFromState = (state: AppNoteModalState): AppNote => ({
+        id: state.id,
+        title: state.title,
+        color: state.color,
+        text: state.text,
+        categories: state.categories,
+        tags: state.tags,
+        expanded: state.expanded,
+    })
 
     private addNote = (): void => {
-        const state: AppNote = this.state;
-        this.props.actions.addNote({
+        const state: AppNoteModalState = this.state;
+        this.props.actions.addNote(this.removeNewTagFromState({
             ...state,
             id: uuid.v4(),
-        });
+        }));
     }
 
     public render() {
@@ -71,24 +114,75 @@ export class NoteModal extends React.Component<AppNoteModalProps & AppNoteModalD
         const colorCheckboxes = this.colors.map((color: string, index: number) => {
             return (
                 <Form.Field key={index}>
-                    <label className={`ui ${color.toLowerCase()} basic label app-checkbox-label`} >
+                    <label
+                        className={`ui ${color}
+                        ${(this.state.color === color) ? '' : 'basic'} label app-checkbox-label`}
+                    >
                         <Checkbox
                             radio={true}
                             name="colorCheckbox"
-                            value={color.toLowerCase()}
-                            checked={this.state.color === color.toLowerCase()}
+                            value={color}
+                            checked={this.state.color === color}
                             onChange={this.handleColorChange}
-                            className="ui red"
                         />
                     </label>
                 </Form.Field>
             );
         });
 
+        const categoriesCheckboxes = this.categoriesList.map((category: string, index: number) => {
+            return (
+                <Form.Field key={index}>
+                    <label
+                        className={`ui basic label app-modal-category-title ${(this.state.categories.includes(category))
+                            ? this.state.color
+                            : ''}`}
+                    >
+                        <Checkbox
+                            name="categoryCheckbox"
+                            value={category}
+                            checked={this.state.categories.includes(category)}
+                            onChange={this.handleCategoryChange}
+                        />
+                        {category}
+                    </label>
+                </Form.Field>
+            );
+        });
+
+        const getTags = (tagsList: string[], deleteIcon: boolean): ReactNode => {
+            const getIcon = (tag: string): ReactNode => (
+                <Icon
+                    name="delete"
+                    className="app-tag-delete-icon"
+                    onClick={() => deleteIcon ? this.handleDeleteTag(tag) : false}
+                />
+            );
+            return tagsList.map((tag: string, index: number) => {
+                return (
+                    <Label
+                        as="a"
+                        key={index}
+                        className={`${this.state.tags.includes(tag) ? 'active' : ''}`}
+                        onClick={() => this.handleTagClick(tag)}
+                    >
+                        {tag}
+                        {(deleteIcon ? getIcon(tag) : '')}
+                    </Label>
+                );
+            });
+        };
+
+        const basicTags = getTags(this.props.tags.basicTags, false);
+
+        const customTags = getTags(this.props.tags.customTags, true);
+
+        const allTags = this.props.tags.basicTags.concat(this.props.tags.customTags);
+
         return (
             <div>
                 <Modal
-                    className="app-note-modal-container"
+                    className={`app-note-modal-container ${this.state.color}`}
                     size="small"
                     open={this.props.modal.opened}
                     onClose={() => {
@@ -103,33 +197,75 @@ export class NoteModal extends React.Component<AppNoteModalProps & AppNoteModalD
                     </Modal.Header>
                     <Modal.Content>
                         <Form>
-                            <Form.Group widths="equal">
-                                <Form.Field required={true} inline={true}>
-                                    <label className={`ui right pointing label`}>Title:&nbsp;
+                            <Form.Group >
+                                    <Label className={`right pointing basic grey app-modal-label`}>Title:
                                         <span className="app-note-form--info">
                                             (max length - {this.maxTitleLength})
                                         </span>
-                                    </label>
+                                    </Label>
                                     <Input
                                         value={this.state.title}
                                         placeholder="Note title"
                                         maxLength={this.maxTitleLength}
                                         onChange={this.handleTitleChange}
+                                        className="app-modal-input"
+
                                     />
-                                </Form.Field>
                             </Form.Group>
-                            <Divider/>
+                            <Divider hidden={true}/>
                             <Form.Group inline={true}>
-                                <label className={`ui right pointing label`}>Color:</label>
+                                <Label className={`right pointing basic grey app-modal-label`}>Color:</Label>
                                 {colorCheckboxes}
                             </Form.Group>
-                            <Divider/>
+                            <Divider hidden={true}/>
+                            <Form.Group inline={true}>
+                                <Label
+                                    className={`right pointing basic grey app-modal-label app-categories`}
+                                >
+                                    Category:
+                                </Label>
+                                {categoriesCheckboxes}
+                            </Form.Group>
+                            <Divider hidden={true}/>
+                            <Form.Group >
+                                <Label className={`right pointing basic grey app-modal-label`}>Tags:</Label>
+                                <Label.Group tag={true}>
+                                    {basicTags}
+                                    {customTags}
+                                </Label.Group>
+                            </Form.Group>
+                            <Divider hidden={true}/>
+                            <Form.Group>
+                                    <Label className={`right pointing basic grey middle aligned app-modal-label`}>
+                                        Custom tag:
+                                    </Label>
+                                    <Input
+                                        placeholder="Add your tag here..."
+                                        value={this.state.newTag}
+                                        maxLength={this.maxNewTagLength}
+                                        className="app-modal-input"
+                                        onChange={this.handleNewTagChange}
+                                    />
+                                    <Button
+                                        className="app-modal-button basic"
+                                        disabled={this.addTagIsDisabled(allTags)}
+                                        onClick={() => {
+                                            this.props.actions.addCustomTag(this.state.newTag);
+                                            this.setState({tags: this.state.tags.concat(this.state.newTag)});
+                                            this.resetNewTag();
+                                        }}
+                                    >
+                                        Add tag
+                                    </Button>
+                            </Form.Group>
+                            <Divider hidden={true}/>
                             <Form.Field inline={true}>
-                                <label className={`ui pointing below label`}>Text:</label>
+                                <Label className={`pointing below basic grey app-modal-label`}>Text:</Label>
                                 <TextArea
                                     value={this.state.text}
-                                    placeholder="Add some interesting note..."
+                                    placeholder="Add some interesting text..."
                                     onChange={this.handleTextChange}
+                                    maxLength={this.maxTextAreaLength}
                                 />
                             </Form.Field>
                         </Form>
@@ -137,11 +273,13 @@ export class NoteModal extends React.Component<AppNoteModalProps & AppNoteModalD
                     <Modal.Actions>
                         <Button
                             onClick={() => {
-                                (noteForUpdate) ? this.props.actions.updateNote(this.state) : this.addNote();
+                                (noteForUpdate)
+                                    ? this.props.actions.updateNote(this.removeNewTagFromState(this.state))
+                                    : this.addNote();
                                 this.resetState();
                                 this.props.actions.closeModal();
                             }}
-                            disabled={this.isDisabled()}
+                            disabled={this.addNoteIsDisabled()}
                             className={`ui ${this.state.color}`}
                         >
                             {(noteForUpdate) ? 'Update' : 'Add'}
