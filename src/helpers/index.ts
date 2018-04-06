c094c455075f28d35961ed289621b3b684b9dc08
severjason <ssever2010@gmail.com>
2018-04-06 12:40:12 +0300

    Added whitespace in comment

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -4,7 +4,7 @@ export function toggleStringInArray(array: string[], value: string): string[] {
     return array.includes(value) ? array.filter((s: string) => s !== value) : array.concat(value);
 }
 
-//concat two arrays without duplicate values
+// concat two arrays without duplicate values
 export function concatArrayUnique(array1: string[], array2: string[]): string[] {
     return array1.concat(array2.filter((s: string) => !array1.includes(s)));
 }
