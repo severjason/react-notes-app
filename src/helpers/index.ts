dd4c1f902707b7f9cbb106c89f0d63f1c063dc87
severjason <ssever2010@gmail.com>
2018-04-06 12:37:08 +0300

    Added comments to helpers

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,7 +1,10 @@
+// if array contains value - removes it from array
+// else - adds it to array
 export function toggleStringInArray(array: string[], value: string): string[] {
     return array.includes(value) ? array.filter((s: string) => s !== value) : array.concat(value);
 }
 
+//concat two arrays without duplicate values
 export function concatArrayUnique(array1: string[], array2: string[]): string[] {
     return array1.concat(array2.filter((s: string) => !array1.includes(s)));
 }
