c0d04a3da9ec8791d141b7ba386c3734664ab00d
severjason <ssever2010@gmail.com>
2018-04-06 12:29:44 +0300

    Added cases for toggle and activate categories

--- src/reducers/categoriesReducer.ts
+++ src/reducers/categoriesReducer.ts
@@ -1,13 +1,28 @@
-// import { types } from '../constants/types';
-import { AppAction, AppCategories } from '../interfaces';
+import { AppAction, AppActionCategory, AppCategories } from '../interfaces';
+import { types } from '../constants/types';
 
 const INITIAL_STATE: AppCategories = {
-    active: 'All',
-    categoriesList: ['All', 'Work', 'Private', 'Health']
+    activated: 'all',
+    categoriesList: ['all', 'work', 'private', 'health'],
+    expanded: true,
 };
 
-export default function categoriesReducer(state: AppCategories = INITIAL_STATE, action: AppAction) {
+export default function categoriesReducer(
+    state: AppCategories = INITIAL_STATE,
+    action: AppAction & AppActionCategory) {
     switch (action.type) {
+        case types.categories.TOGGLE_CATEGORIES: {
+            return {
+                ...state,
+                expanded: !state.expanded,
+            };
+        }
+        case types.categories.ACTIVATE_CATEGORY: {
+            return {
+                ...state,
+                activated: (state.categoriesList.includes(action.category) ? action.category : state.activated),
+            };
+        }
         default: {
             return state;
         }
