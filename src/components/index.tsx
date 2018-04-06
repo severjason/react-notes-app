cac56a3ac4ad63f06a6fa9c3aa455342b90a01de
severjason <ssever2010@gmail.com>
2018-04-06 12:42:35 +0300

    Removed CategoriesList from index

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -2,12 +2,10 @@ import Header from './Header';
 import NotesList from './NotesList';
 import Note from './Note';
 import EmptyPage from './EmptyPage';
-import CategoriesList from './CategoriesList';
 
 export {
     Header,
     NotesList,
     Note,
     EmptyPage,
-    CategoriesList,
 };
