abc159cb16a1c17752d8846060a97efc0d6ac35b
severjason <ssever2010@gmail.com>
2018-04-08 00:31:04 +0300

    Empty page styling

--- src/components/EmptyPage/index.tsx
+++ src/components/EmptyPage/index.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import './index.css';
 
 interface AppEmptyPage {
     category?: string;
@@ -6,8 +7,8 @@ interface AppEmptyPage {
 
 const EmptyPage: React.StatelessComponent<AppEmptyPage> = (props: AppEmptyPage) => {
     return (
-        <div>
-            {(props.category === 'all') ? `No notes...` : `No notes in ${props.category} category...`}
+        <div className="ui compact message app-empty-page-message-container">
+            {(props.category === 'all') ? `No notes...` : `No notes in this category...`}
         </div>
     );
 };
