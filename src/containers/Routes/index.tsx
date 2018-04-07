c92a8505bc79abe40efad6b26dbd065ecc4776ee
severjason <ssever2010@gmail.com>
2018-04-07 17:57:30 +0300

    Default route changed from /notes to /

--- src/containers/Routes/index.tsx
+++ src/containers/Routes/index.tsx
@@ -5,9 +5,8 @@ import { Home } from '../../containers';
 const Routes: React.StatelessComponent<{}> = () => {
     return (
         <Switch>
-            <Route path="/notes" component={Home}/>
+            <Route path="/" component={Home}/>
             <Route exact={true} path="/" render={() => <Redirect to="/notes/all"/>}/>
-            <Route path="*" render={() => <Redirect to="/notes/all"/>}/>
         </Switch>
     );
 };
